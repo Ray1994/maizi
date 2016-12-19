90c49ef5586a93289c4ce175fb5664101e70407d
Ray1994 <865583900@qq.com>
2016-12-19 23:51:55 +0800

    1
    11

---
+++ js/maizi.js
@@ -0,0 +1,11 @@
+    $(function() {
+    	/*导航跳转效果插件*/
+    	$('.nav').singlePageNav({
+    		offset: 70
+    	});
+    	/*小屏幕导航点击关闭菜单*/
+    	$('.navbar-collapse a').click(function() {
+    		$('.navbar-collapse').collapse('hide');
+    	});
+    	new WOW().init();
+    })
